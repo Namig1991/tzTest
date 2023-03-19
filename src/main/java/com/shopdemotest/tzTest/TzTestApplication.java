59a7cbd5d52577d9aee935c47a45b05ce199d8a1
Namig1991 <safarovnamig09@gmail.com>
2023-03-19 16:44:40 +0300

    First commit

---
+++ src/main/java/com/shopdemotest/tzTest/TzTestApplication.java
@@ -0,0 +1,13 @@
+package com.shopdemotest.tzTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TzTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TzTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopdemotest/tzTest/TzTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopdemotest.tzTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TzTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
